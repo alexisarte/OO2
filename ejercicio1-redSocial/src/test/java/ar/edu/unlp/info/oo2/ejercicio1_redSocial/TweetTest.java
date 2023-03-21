ad4515d0b8d8160b1584c5a7519f677d1712ad7b
alexis <alexisrodriguezarteaga@gmail.com>
2023-03-20 21:50:00 -0300

    Agrego estructura del ejercicio 1

---
+++ ejercicio1-redSocial/src/test/java/ar/edu/unlp/info/oo2/ejercicio1_redSocial/TweetTest.java
@@ -0,0 +1,14 @@
+package ar.edu.unlp.info.oo2.ejercicio1_redSocial;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TweetTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
