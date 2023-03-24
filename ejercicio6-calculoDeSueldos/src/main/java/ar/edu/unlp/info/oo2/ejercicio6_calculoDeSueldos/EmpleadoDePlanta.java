fb365077f334dadf875dd7200a6a011d8297e439
alexis <alexisrodriguezarteaga@gmail.com>
2023-03-24 19:19:02 -0300

    Ejercicio 6

---
+++ ejercicio6-calculoDeSueldos/src/main/java/ar/edu/unlp/info/oo2/ejercicio6_calculoDeSueldos/EmpleadoDePlanta.java
@@ -0,0 +1,28 @@
+package ar.edu.unlp.info.oo2.ejercicio6_calculoDeSueldos;
+
+public class EmpleadoDePlanta extends Empleado {
+
+	private boolean estaCasado;
+	private int cantidadDeHijos;
+	private int antiguedad;
+
+	public EmpleadoDePlanta(String nombre, String apellido, boolean estaCasado, int cantidadDeHijos,
+			int aniosDeAntiguedad) {
+		super(nombre, apellido);
+		this.estaCasado = estaCasado;
+		this.cantidadDeHijos = cantidadDeHijos;
+	}
+
+	@Override
+	public double sueldoBasico() {
+		return 50000;
+	}
+
+	@Override
+	public double adicionales() {
+		double monto = 0;
+		monto += this.estaCasado ? 5000 : 0;
+		return monto + (this.cantidadDeHijos * 2000) + (2000 * this.antiguedad);
+	}
+
+}
