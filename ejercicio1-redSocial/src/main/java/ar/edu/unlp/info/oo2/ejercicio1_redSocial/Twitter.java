fcac3a6044c8e3c05644d03ea626d29d94a1ef53
alexis <alexisrodriguezarteaga@gmail.com>
2023-03-26 19:34:08 -0300

    Agrego resolucion del ejercicio 1

--- ejercicio1-redSocial/src/main/java/ar/edu/unlp/info/oo2/ejercicio1_redSocial/Twitter.java
+++ ejercicio1-redSocial/src/main/java/ar/edu/unlp/info/oo2/ejercicio1_redSocial/Twitter.java
@@ -1,5 +1,33 @@
 package ar.edu.unlp.info.oo2.ejercicio1_redSocial;
 
-public class Twitter {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
+public class Twitter {
+	
+	private List<Usuario> usuarios;
+	
+	public Twitter() {
+		this.usuarios = new ArrayList<>();
+	}
+	
+	public void registrarUsuario(Usuario usuario) {
+		this.usuarios.add(usuario);
+	}
+
+	public void eliminarUsuario(Usuario usuario) {
+		usuario.eliminarTweets();
+		this.usuarios.remove(usuario);
+	}
+
+	public boolean existeUsuario(Usuario usuario) {
+		return this.usuarios.stream().anyMatch(u -> u.getScreenName() == usuario.getScreenName());
+	}
+	
+	// retorna una nueva lista "no modificable" que comparte los mismos objetos que la lista original
+	public List<Usuario> getUsuarios() {
+		return Collections.unmodifiableList(this.usuarios);
+	}
+	
 }
--- ejercicio1-redSocial/src/test/java/ar/edu/unlp/info/oo2/ejercicio1_redSocial/TwitterTest.java
+++ ejercicio1-redSocial/src/test/java/ar/edu/unlp/info/oo2/ejercicio1_redSocial/TwitterTest.java
@@ -2,13 +2,58 @@ package ar.edu.unlp.info.oo2.ejercicio1_redSocial;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class TwitterTest {
-
-	@Test
-	void test() {
-		fail("Not yet implemented");
+	
+	Twitter twitter;
+    Usuario usuario1, usuario2, usuario3;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		twitter = new Twitter();
+        usuario1 = new Usuario("usuario1");
+        usuario2 = new Usuario("usuario2");
+        usuario3 = new Usuario("usuario3");
+        twitter.registrarUsuario(usuario1);
+        twitter.registrarUsuario(usuario2);
 	}
+	
+	@Test
+    void testRegistrarUsuario() {
+        twitter.registrarUsuario(usuario3);
+        
+        assertTrue(twitter.existeUsuario(usuario3));
+    }
+	
+	public void testEliminarUsuario() {
+		usuario2.postearTweet("Hola, este es mi primer tweet");
+		usuario2.postearTweet("Estoy probando la red social Twitter");
+        
+        twitter.eliminarUsuario(usuario2);
+        
+        assertFalse(twitter.existeUsuario(usuario2));
+        assertEquals(1, twitter.getUsuarios().size());
+        assertTrue(usuario1.getTweets().isEmpty());
+    }
 
+    @Test
+    void testExisteUsuario() {
+        assertTrue(twitter.existeUsuario(usuario1));
+        
+        twitter.eliminarUsuario(usuario1);
+        
+        assertFalse(twitter.existeUsuario(usuario1));
+    }
+    
+    @Test
+    void getUsuarios() {
+    	var usuarios = twitter.getUsuarios();
+        assertTrue(usuarios.contains(usuario1) && usuarios.contains(usuario2));
+        assertEquals(2, usuarios.size());
+    }
+    
 }
+
+
