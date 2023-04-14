b7f2f7f80e4244df008efd3d6b97d025b2d68900
alexis <alexisrodriguezarteaga@gmail.com>
2023-04-13 21:35:42 -0300

    Pattern Strategy and Adapter

---
+++ ejercicio11-dispositivoMovilYConexiones/src/main/java/ar/edu/unlp/info/oo2/ejercicio11_dispositivoMovilYConexiones/CRC_Calculator.java
@@ -0,0 +1,7 @@
+package ar.edu.unlp.info.oo2.ejercicio11_dispositivoMovilYConexiones;
+
+public interface CRC_Calculator {
+    
+	long crcFor(String data);
+
+}
---
+++ ejercicio11-dispositivoMovilYConexiones/src/main/java/ar/edu/unlp/info/oo2/ejercicio11_dispositivoMovilYConexiones/Display.java
@@ -0,0 +1,9 @@
+package ar.edu.unlp.info.oo2.ejercicio11_dispositivoMovilYConexiones;
+
+public class Display {
+	
+    public String showBanner(String img) {
+    	return null;
+    }
+
+}
